f49e6fd94f0ac3bc6725f6e3f6d8a743db2d6851
gtankha <46304828+gtankha@users.noreply.github.com>
2020-09-17 13:49:40 -0700

    final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
+//const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 
 //const profileDataArgs = process.argv.slice(2);
 //
@@ -153,8 +154,18 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
-     });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
